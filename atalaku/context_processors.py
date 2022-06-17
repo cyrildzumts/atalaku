26829067f9b98fc494eb33571ee2d1e2e89f0a9e
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2022-06-17 21:25:50 +0200

    wip

--- atalaku/context_processors.py
+++ atalaku/context_processors.py
@@ -2,10 +2,14 @@ from atalaku import settings
 
 
 def site_context(request):
+    is_dashboard_allowed = False
+    if request.user.is_authenticated:
+        is_dashboard_allowed = request.user.has_perm('dashboard.can_view_dashboard')
     context = {
         'site_name' : settings.SITE_NAME,
         'meta_keywords': settings.META_KEYWORDS,
         'meta_description': settings.META_DESCRIPTION,
+        'is_dashboard_allowed': is_dashboard_allowed,
         'redirect_to' : '/'
     }
     return context

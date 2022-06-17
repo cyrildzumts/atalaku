20e3196318dd1e933c93579dfc084abe2f4ee042
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2022-06-17 16:52:56 +0200

    wip

--- atalaku/urls.py
+++ atalaku/urls.py
@@ -14,23 +14,30 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls import url, include
 from django.urls import path
-from django.conf import settings
+from atalaku import settings
+from django.contrib.sitemaps.views import sitemap
 from django.conf.urls.static import static
 from atalaku import views
 
 
-urlpatterns = [
-    path('', views.home, name='home'),
+urlpatterns_i18n = i18n_patterns( * [
+    path('', views.home, name="home"),
     path('about/', views.about, name='about'),
     path('accounts/', include('accounts.urls')),
-    path('admin/', admin.site.urls),
     path('events/', include('events.urls')),
-    path('dashboard/', include('dashboard.urls')),
     path('faq/', views.faq, name='faq'),
-    path('index/', views.home, name='index'),
-    path('i18n/', include('django.conf.urls.i18n')),
+    path('dashboard/', include('dashboard.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+], prefix_default_language=False)
+
+urlpatterns = [
+    path('privacy-policy/', views.privacy_policy, name="privacy-policy"),
+    #path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
+    path('terms-of-use/', views.terms_of_use, name="terms-of-use"),
+    path('i18n/', include('django.conf.urls.i18n')),
+    *urlpatterns_i18n
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
